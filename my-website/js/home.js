62f67c5bbeb4bc49e9751c16d9c5f5ddac2ee23c
yogiez14 <acsyonezo@gmail.com>
2025-04-27 18:29:15 -0600

    Update home.js

--- my-website/js/home.js
+++ my-website/js/home.js
@@ -1,4 +1,4 @@
-const API_KEY = 'a1e72fd93ed59f56e6332813b9f8dcae';
+const API_KEY = '3de1b3a6848d44e7246c0748d67087bf';
     const BASE_URL = 'https://api.themoviedb.org/3';
     const IMG_URL = 'https://image.tmdb.org/t/p/original';
     let currentItem;
